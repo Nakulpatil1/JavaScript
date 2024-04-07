2a00819fe9588cc2b121ac87899cf53bba3c5339
Nakul Patil <143025294+Nakulpatil1@users.noreply.github.com>
2024-04-07 07:46:39 +0000

    Understanding DataTypes

---
+++ 1.Basics/2.dataType.js
@@ -0,0 +1,16 @@
+let name = "nakul"; //String Datatype
+let age = 20; //Number Datatype
+let isLoggedIn = true; //Boolean Datatype
+
+//number range = 2 to power 53
+//string = ""
+//boolean = true or false
+//null = standalone value
+//undefined 
+//symbol = unique 
+//object 
+
+console.log(typeof 18)         //number
+console.log(typeof "Nakul")    //string
+console.log(typeof null)       //object
+console.log(typeof undefined)  //undefined
