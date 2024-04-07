43e90c0d3d895ab82d231a7c52f96d187e144b23
Nakul Patil <143025294+Nakulpatil1@users.noreply.github.com>
2024-04-07 07:20:00 +0000

    Basic Variable Undderstanding

---
+++ 1.Basics/variables.js
@@ -0,0 +1,9 @@
+let accountID = "12334"
+var accountEmail = "ahah@gmail.com"
+const accountName = "Nakul"
+let accountPassword
+
+// Prefer Not to use var.
+// Because of issue in block scope and functionaol Scope.
+
+console.table([accountID, accountEmail, accountName, accountPassword])
