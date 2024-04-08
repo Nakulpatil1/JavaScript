c5909f629d79ea82d781fede35fe5a2d0498daab
Nakul Patil <143025294+Nakulpatil1@users.noreply.github.com>
2024-04-08 06:24:14 +0000

    DataType Conversion

---
+++ 1.Basics/3.datatypeConversion.js
@@ -0,0 +1,24 @@
+let marks = "50"
+
+console.log(marks)
+console.log(typeof marks)
+
+let value = Number (marks)   //Type Conversion
+console.log(typeof value)    
+
+//Conversion to Number
+//"33" = 33
+//"33acb" = NaN
+//true = 1;
+//false = 0;
+
+let isLoggedIn = 1;
+
+let booleanisLoggedIn = Boolean (isLoggedIn)
+console.log(booleanisLoggedIn)
+
+//Conversion to Boolean 
+//1 = true
+//0 = false
+//"" = false
+//"abc" = true
