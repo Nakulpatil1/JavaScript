2c9704818607045d7b0ba05852c5883ae260cb67
Nakul Patil <143025294+Nakulpatil1@users.noreply.github.com>
2024-04-09 19:25:47 +0000

    Understanding nums and maths

---
+++ 1.Basics/7.numsAndmaths.js
@@ -0,0 +1,18 @@
+let num1 = 28.87
+console.log(num1)
+
+let num2 = new Number (78)
+console.log(num2)
+
+//Different Methods
+
+console.log(num1.toString().length) //Length of the value
+console.log(num1.toPrecision(2))    //Precise no. of Output
+console.log(num1.toFixed(1))        //Fixed no. of Output
+
+let zero = 10000000000;
+console.log(zero.toLocaleString())  //Number in String
+console.log(zero.toLocaleString("en-In"))  //Indian Standard
+
+
+
