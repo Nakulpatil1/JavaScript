f00c77b44d752e20146c813604c0fbc758c9afbd
Nakul Patil <143025294+Nakulpatil1@users.noreply.github.com>
2024-04-14 09:16:36 +0000

    Understanding Switch

---
+++ 4.Controlflow/switch.js
@@ -0,0 +1,35 @@
+//Switch Case Statment
+
+// switch (key) {      //Syntax
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+
+const month = 3
+switch (month) {      
+    case 1: 
+        console.log("Month is January");
+        break;
+    case 2: 
+        console.log("Month is February");
+        break;
+    case 3: 
+        console.log("Month is march");
+        break;
+    case 4: 
+        console.log("Month is April");
+        break;
+    default:
+        console.log("Default Case");
+        break;
+}
+
+
+
+
