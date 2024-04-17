f5e4b1ea1ae8430f01df10d3284b6f8a104a1529
Nakul Patil <143025294+Nakulpatil1@users.noreply.github.com>
2024-04-17 18:15:27 +0000

    Understanding for-of loop

---
+++ 5.iteration.js/for-of_loop.js
@@ -0,0 +1,38 @@
+//for-of loop 
+
+// ["", "", ""]
+// [{}, {}, {}]
+
+// for (const iterator of object) {      //Syntax
+    
+// }
+
+
+const greetings = "Hello world!"
+for (const greet of greetings) {
+    console.log(`Each char is ${greet}`)
+}
+
+
+
+const map = new Map()
+map.set('IN', "India")
+map.set('USA', "United States of America")
+map.set('Fr', "France")
+map.set('IN', "India")
+console.log(map);
+
+for (const [key, value] of map) {
+    console.log(key, ':-', value);
+}
+
+
+
+const myObject = {
+    game1: 'NFS',
+    game2: 'Spiderman'
+}
+for (const [key, value] of myObject) {
+    console.log(key, ':-', value);
+    
+}
